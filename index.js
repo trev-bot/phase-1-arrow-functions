b768950d28278538579f2c6978785077143c580e
Trevor <duncantrev@gmail.com>
2021-12-19 12:39:55 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function (){
+   return (2000 / 100 )
+}
+
+const square = (parameter) => parameter * parameter;
+square(parameter, parameter);
+
+const add = (a, b) => {
+    a + b
+   };
+
+add (3,4);
